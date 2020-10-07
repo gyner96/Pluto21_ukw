ae78d2df86177f06b9f0452dc9f5af92e147794b
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2020-10-07 13:52:36 +0200

    Class after S1

---
+++ app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
@@ -0,0 +1,16 @@
+package de.hawlandshut.pluto21_ukw;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "xx MainActivity";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate called");
+        setContentView(R.layout.activity_main);
+    }
+}
