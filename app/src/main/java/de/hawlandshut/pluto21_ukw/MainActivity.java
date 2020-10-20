3341296f86e2348211eba819dcb6edf35a27988e
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2020-10-20 09:20:19 +0200

    Minor

--- app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
@@ -7,10 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "xx MainActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate called");
         setContentView(R.layout.activity_main);
     }
+
 }
