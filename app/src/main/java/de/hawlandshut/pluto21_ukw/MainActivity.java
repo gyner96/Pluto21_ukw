226bec9eb6d3097cc08310092246b4eda28b1388
dgreipl <dgreipl@hawlandshutde.onmicrosoft.com>
2020-10-21 10:58:55 +0200

    added lifecycle

--- app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
+++ app/src/main/java/de/hawlandshut/pluto21_ukw/MainActivity.java
@@ -1,8 +1,10 @@
 package de.hawlandshut.pluto21_ukw;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,4 +18,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy called");
+    }
 }
